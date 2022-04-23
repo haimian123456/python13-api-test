1a01664eb77c21893d576ae250111dd294710627
haimian123456 <1254300664@qq.com>
2022-04-23 18:53:03 +0800

    test suite

--- common/test_suite.py
+++ common/test_suite.py
@@ -1,14 +1,11 @@
 
-from testcases.test_register import Register_Test
+
 from common.contants import *
 import os
 import unittest
 import HTMLTestRunnerNew
-from common.context import *
 
-# suite_register=unittest.TestSuite()
-# loader=unittest.TestLoader()
-# suite_register.addTest(loader.loadTestsFromTestCase(Register_Test))
+
 
 discover=unittest.defaultTestLoader.discover(testcases_dir,"test_*.py")
 reports=os.path.join(reports_dir,'reports.html')
